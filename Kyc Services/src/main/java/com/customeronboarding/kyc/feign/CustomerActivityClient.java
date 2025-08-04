8877477fad6887ca220df598c0a735f69c625f9b
Sathwik Vintha <sathwikvintha7389@gmail.con>
2025-08-04 10:32:46 +0530

    Added audit logs for Admin End Points

--- Kyc Services/src/main/java/com/customeronboarding/kyc/feign/CustomerActivityClient.java
+++ Kyc Services/src/main/java/com/customeronboarding/kyc/feign/CustomerActivityClient.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "customer-service", url = "${CUSTOMER_SERVICE_URL}") // or service discovery
+@FeignClient(name = "customer-service", url = "${CUSTOMER_SERVICE_URL}")
 public interface CustomerActivityClient {
 
-    @PostMapping("/api/customers/{id}/activity-log")
-    void logActivity(@PathVariable("id") Long customerId,
-                     @RequestParam("action") String action,
+    @PostMapping("/api/customers/activity")
+    void logActivity(@RequestParam("customerId") Long customerId,
+                     @RequestParam("activityType") String activityType,
                      @RequestParam("description") String description);
 }
